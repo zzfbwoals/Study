fbcd0bd90962569aa6d4b54538d79595e88aaff4
류재민 <ryumin0421@gmail.com>
2024-08-09 14:57:47 +0900

    조건문 변경

--- Step/Conditional Statement/6/오븐 시계.cpp
+++ Step/Conditional Statement/6/오븐 시계.cpp
@@ -7,23 +7,14 @@ int main()
     int A, B, C; // (0 ≤ A ≤ 23), (0 ≤ B ≤ 59), (0 ≤ C ≤ 1,000)
     cin >> A >> B;
     cin >> C;
-    if ( B+C < 60 )
+    if ( B+C < 60 ) // B+C가 60분 미만인 경우
         cout << A << " " << B+C << endl;
-    else if ( B+C >= 60 && B+C < 120 )
+    else if ( B+C >= 60 ) // B+C가 60분 이상인 경우
     {
-        if ( A+1 == 24 )
-            cout << 0 << " " << B+C-60 << endl;
-        else
-            cout << A+1 << " " << B+C-60 << endl;
-    }
-    else if ( B+C >= 60 && B+C >= 120 )
-    {
-        if ( A+((B+C)/60) == 24 )
+        if ( A+((B+C)/60) == 24 ) // A가 24시인 경우
             cout << 0 << " " << (B+C)%60 << endl;
-        else
+        else // A가 24시가 아닌 경우
             cout << A+((B+C)/60) << " " << (B+C)%60 << endl;
     }
-    else
-        cout << 0 << " " << B+C-60 << endl;
     return 0;
 }
