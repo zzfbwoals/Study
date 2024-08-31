a203d90e78657014bb2d23a4e822825a3886272c
류재민 <ryumin0421@gmail.com>
2024-08-31 13:06:40 +0900

    2024.08.31

---
+++ Step/GeneralMath/6/분수찾기.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    1/1
+    2/1 1/2
+    3/1 2/2 1/3
+    4/1 3/2 2/3 1/4
+    5/1 4/2 3/3 2/4 1/5
+
+    n/1
+    n/1 n-1/n+1
+    n/1 n-1/n+1 n-2/n+2
+    */
+    int X;
+    cin >> X;
+
+    return 0;
+}
