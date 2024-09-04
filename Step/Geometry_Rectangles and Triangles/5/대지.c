41fd9311f6c3c5ae73fdc74cce7ed12765c6fb40
류재민 <ryumin0421@gmail.com>
2024-09-04 22:58:30 +0900

    2024.09.04

---
+++ Step/Geometry_Rectangles and Triangles/5/대지.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <limits.h>
+
+struct XY {
+    int x;
+    int y;
+};
+
+int main()
+{
+    // 값 입력
+    int N;
+    scanf("%d", &N);
+    struct XY *xy = (struct XY*)malloc(N * sizeof(struct XY));
+    for(int i = 0; i < N; i++)
+    {
+        scanf("%d %d", &xy[i].x, &xy[i].y);
+    }
+
+    // xy 최소 최대 찾기
+    int minX = INT_MAX, maxX = INT_MIN, minY = INT_MAX, maxY = INT_MIN; // 최소값은 정수형 최대값으로 초기화, 최대값은 정수형 최소값으로 초기화
+    for(int i = 0; i < N; i++)
+    {
+        if(minX > xy[i].x)
+            minX = xy[i].x;
+        if(maxX < xy[i].x)
+            maxX = xy[i].x;
+        if(minY > xy[i].y)
+            minY = xy[i].y;
+        if(maxY < xy[i].y)
+            maxY = xy[i].y;
+    }
+
+    int area = (maxX - minX) * (maxY - minY);
+    printf("%d\n", area);
+
+    free(xy);
+    return 0;
+}
