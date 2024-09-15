7eb98456ce1390d8cb89f1dc77fc05f1865dc206
ryu jaemin <ryumin0421@gmail.com>
2024-09-15 16:32:45 +0900

    2024.09.15 선언부 구현부 구분

--- C++Programming/실습문제/3장/1.cpp
+++ C++Programming/실습문제/3장/1.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
 using namespace std;
 
+// 선언부
 class Tower {
     private:
         int height;
     public:
-        Tower() {
-            height = 1;
-        };
-        Tower(int num) {
-            height = num;
-        };
-        int getHeight() {
-            return height;
-        };
+        Tower();
+        Tower(int num);
+        int getHeight();
 };
 
+// 구현부
+Tower::Tower() {
+    height = 1;
+}
+
+Tower::Tower(int num) {
+    height = num;
+}
+
+int Tower::getHeight() {
+    return height;
+}
+
+// 메인함수
 int main()
 {
     Tower myTower;
